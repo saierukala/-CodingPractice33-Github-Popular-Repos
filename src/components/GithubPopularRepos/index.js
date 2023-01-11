39f50bef1eed4ee99f4605ddf2aed12578deaefe
saierukala <119946503+saierukala@users.noreply.github.com>
2023-01-11 12:38:51 +0530

    Update index.js

--- src/components/GithubPopularRepos/index.js
+++ src/components/GithubPopularRepos/index.js
@@ -1,12 +1,3 @@
-const languageFiltersData = [
-  {id: 'ALL', language: 'All'},
-  {id: 'JAVASCRIPT', language: 'Javascript'},
-  {id: 'RUBY', language: 'Ruby'},
-  {id: 'JAVA', language: 'Java'},
-  {id: 'CSS', language: 'CSS'},
-]
-
-// Write your code here
 import {Component} from 'react'
 import Loader from 'react-loader-spinner'
 
